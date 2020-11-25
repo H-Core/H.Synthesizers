1441cd2feb69b441a8f07d189e57277b25506250
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-25 14:36:01 +1000

    Initialized.

---
+++ src/libs/H.Synthesizers.WebSynthesizers/CachedSynthesizer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core;
+using H.Core.Synthesizers;
+
+namespace H.Synthesizers
+{
+    public abstract class CachedSynthesizer : Synthesizer, ISynthesizer
+    {
+        #region Properties
+
+        private ByteArrayCache Cache { get; }
+
+        #endregion
+
+        #region Constructors
+
+        protected CachedSynthesizer()
+        {
+            UseCache = true;
+            Cache = new ByteArrayCache(GetType());
+        }
+
+        #endregion
+
+        #region ISynthesizer
+
+        public async Task<byte[]> ConvertAsync(string text, CancellationToken cancellationToken = default)
+        {
+            var key = TextToKey(text);
+            if (UseCache && Cache.Contains(key))
+            {
+                return Cache[key]?.ToArray() ?? Array.Empty<byte>();
+            }
+
+            var bytes = await InternalConvertAsync(text, cancellationToken).ConfigureAwait(false);
+            Cache[key] = bytes;
+            return bytes;
+        }
+
+        protected abstract Task<byte[]> InternalConvertAsync(string text, CancellationToken cancellationToken = default);
+
+        protected abstract string TextToKey(string text);
+
+        #endregion
+    }
+}
