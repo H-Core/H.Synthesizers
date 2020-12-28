5e07b0572b79b4357d5f322c065f6e1e75000da4
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 00:34:20 +1000

    Fixed tests.

--- src/tests/H.Synthesizers.WebSynthesizers.IntegrationTests/SimpleTests.cs
+++ src/tests/H.Synthesizers.WebSynthesizers.IntegrationTests/SimpleTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using H.Recorders;
 using H.Recorders.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NAudio.Wave;
@@ -10,11 +12,28 @@ namespace H.Synthesizers.WebSynthesizers.IntegrationTests
     [TestClass]
     public class SimpleTests
     {
+        private static void CheckDevices()
+        {
+            var devices = NAudioRecorder.GetAvailableDevices().ToList();
+            if (!devices.Any())
+            {
+                Assert.Inconclusive("No available devices for NAudioRecorder.");
+            }
+
+            Console.WriteLine("Available devices:");
+            foreach (var device in devices)
+            {
+                Console.WriteLine($" - Name: {device.ProductName}, Channels: {device.Channels}");
+            }
+        }
+
         [TestMethod]
         public async Task ConvertTest()
         {
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
             var cancellationToken = cancellationTokenSource.Token;
+
+            CheckDevices();
 
             using var synthesizer = new YandexSynthesizer
             {
